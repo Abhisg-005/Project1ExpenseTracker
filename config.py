228b3bd3ea404749747f751946db4ba11fd22be6
Abhisg-005 <abhiabhisheksg8114@gmail.com>
2026-07-30 18:41:37 +0530

    saved changes

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
-    DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_PORT = int(os.getenv("DB_PORT", "3306"))
-    DB_USER = os.getenv("DB_USER", "root")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "Abhisql@#1")
-    DB_NAME = os.getenv("DB_NAME", "expense_tracker")
+    DB_HOST = os.getenv("DB_HOST", "DB_HOST")
+    DB_PORT = int(os.getenv("DB_PORT", "DB_PORT"))
+    DB_USER = os.getenv("DB_USER", "DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "DB_PASSWORD")
+    DB_NAME = os.getenv("DB_NAME", "DB_NAME")
